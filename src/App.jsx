6d1876bc5c96e2bbe14455c610fa2d190a759bc4
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-08-21 22:19:41 +0530

    Hero added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <>
       <div className="w-full font-code overflow-x-hidden">
         <Navbar />
+        <Hero />
       </div>
     </>
   );
