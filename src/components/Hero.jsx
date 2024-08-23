79b8abdb20d23ce5ace537a76603368cecd0fce3
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-08-23 23:07:41 +0530

    hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='flex justify-center items-center'>
-      <div></div>
+    <div className='flex justify-center w-full h-auto md:min-h-screen items-center bg-blue-900'>
+      <div className='w-[300px] rounded-3xl bg-rose-700 h-[400px]'>Wakata</div>
     </div>
   )
 }
