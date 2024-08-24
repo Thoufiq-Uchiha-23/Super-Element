bc31ffdb684e6705f18365a62dafeeb748b527df
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-08-24 22:54:46 +0530

    smol change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Hero = () => {
   return (
     <div className='flex justify-center w-full h-auto md:min-h-screen items-center bg-blue-900'>
-      <div className='w-[300px] rounded-3xl bg-rose-700 h-[400px]'>Wakata</div>
+      <div className='w-[300px] rounded-3xl bg-rose-700 h-[400px] px-5 py-3'>Wakata</div>
     </div>
   )
 }
