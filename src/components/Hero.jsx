21cd8385198f56bff1a5bd51d85f9c5c32a34a30
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-08-25 23:18:38 +0530

    hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const Hero = () => {
   return (
     <div className='flex justify-center w-full h-auto md:min-h-screen items-center bg-blue-900'>
-      <div className='w-[300px] rounded-3xl bg-rose-700 h-[400px] px-5 py-3'>Wakata</div>
+      <div className='w-[300px] rounded-3xl bg-rose-700 h-[400px] px-5 py-3'>
+        <h1>Card Image</h1>
+        <p>Card char name</p>
+        <p>Card summary</p>
+      </div>
     </div>
   )
 }
