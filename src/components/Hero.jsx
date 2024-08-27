a66fb91a92ed3f0011cce21c7d3a45b388e8d9cf
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-08-27 22:50:05 +0530

    image area

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,8 @@ const Hero = () => {
   return (
     <div className='flex justify-center w-full h-auto md:min-h-screen items-center bg-blue-900'>
       <div className='w-[300px] rounded-3xl bg-rose-700 h-[400px] px-5 py-3'>
-        <h1>Card Image</h1>
+        {/* Card Image */}
+        <img src='' alt='' className='bg-blue-900 w-full h-[300px]'/>
         <p>Card char name</p>
         <p>Card summary</p>
       </div>
