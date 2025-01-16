e46e3f6b54b686c2f4f651d12d40b598df982195
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-01-16 23:43:36 +0530

    minor change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,17 @@
-import React, { useState, useEffect, useRef, useLayoutEffect } from "react";
+// import React, { useState, useEffect, useRef, useLayoutEffect } from "react";
 // import { brand, navigation } from "../constants";
 
 const Navbar = () => {
   //   const pathname = useLocation();
-  const [isOpen, setIsOpen] = useState(false);
+  // const [isOpen, setIsOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
+  // const toggleMenu = () => {
+  //   setIsOpen(!isOpen);
+  // };
 
   return (
-    <div className="w-full h-20 bg-rose-900 flex items-center justify-between">
-      <h1 className="text-6xl text-center font-semibold">SUPER ELEMENT</h1>
+    <div className="w-full h-20 bg-zinc-800 flex items-center justify-between">
+      <h1 className="text-6xl text-center font-semibold bg-clip-text bg-gradient-to-r from-red-700 to-blue-900 text-transparent">SUPER ELEMENT</h1>
       <div className="flex justify-between items-center gap-3">
         <a href="/">Home</a>
         <a href="/">Characters</a>
